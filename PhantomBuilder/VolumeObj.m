050f27479b0dc6f99ea155f4ceb7c64fb6b5f1b6
Tobias Becher <44724749+tobiasbecher@users.noreply.github.com>
2023-02-09 16:51:25 +0100

    Phantom Builder
    Continued with phantom builder

--- PhantomBuilder/VolumeObj.m
+++ PhantomBuilder/VolumeObj.m
@@ -9,9 +9,11 @@ classdef (Abstract) VolumeObj
         Priority = 1;
         Visible = 1;
         visibleColor = [0 0 0];
-        center = [0,0,0]; %center of objective
+        offset = [0,0,0]; %center of objective
         DoseObjectiveFunction;
-        
+        colors = [[1,0,0];[0,1,0];[0,0,1];[1,1,0];[1,0,1];[0,1,1];[1,1,1]];
+
+
     end
     methods (Static, Access = private)
         function oldValue = getOrIncrementCount(increment)
@@ -33,14 +35,27 @@ classdef (Abstract) VolumeObj
         end
     end
     methods
-        function obj = VolumeObj(name,type)
+        function obj = VolumeObj(name,type,DoseObjectiveFunction,offset)
         % Increment the counter in the constructor
             VolumeObj.getOrIncrementCount(1);
             obj.idx = VolumeObj.getOrIncrementCount();
+            if obj.idx <= size(obj.colors,1)
+                obj.visibleColor = obj.colors(obj.idx,:)
+            else
+                obj.visibleColor = [1 1 1];
+            end
+            obj.Priority = obj.idx;
             obj.name = name;
             obj.type = type;
+            obj.offset = offset;
+            %idea is that DoseObjectiveFunction can be either a single objective or an 
+            %array of objectives. If it is a single objective store it as an array 
+            if iscell(DoseObjectiveFunction)
+                obj.DoseObjectiveFunction = DoseObjectiveFunction;
+            else 
+                obj.DoseObjectiveFunction = {DoseObjectiveFunction};
+            end
         end
-        
         function cst = initializeParameters(obj,cst)
             %initialize cst file
             cst{obj.idx,1}                = obj.idx-1;
@@ -52,7 +67,9 @@ classdef (Abstract) VolumeObj
             cst{obj.idx,5}.Priority       = obj.Priority;
             cst{obj.idx,5}.Visible        = obj.Visible;
             cst{obj.idx,5}.visibleColor   = obj.visibleColor;
-                            
+            for i = numel(obj.DoseObjectiveFunction)
+                cst{obj.idx,6} {i}= obj.DoseObjectiveFunction{i} 
+            end
         end
     end
 end
