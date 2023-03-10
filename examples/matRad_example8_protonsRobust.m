c4c599fc327a48d46e9d6a6b3bd0d8a5daf4d4cd
Tobias Becher <44724749+tobiasbecher@users.noreply.github.com>
2023-03-10 10:13:13 +0100

    Update matRad_example8_protonsRobust.m
    Fixed small bug

--- examples/matRad_example8_protonsRobust.m
+++ examples/matRad_example8_protonsRobust.m
@@ -48,7 +48,7 @@ ixOAR    = 3;
 % define general VOI properties
 cst{ixNT,1} = 0;     cst{ixNT,2} = 'contour';    cst{ixNT,3} = 'OAR';
 cst{ixTarget,1} = 1; cst{ixTarget,2} = 'target'; cst{ixTarget,3} = 'TARGET';
-cst{ixOAR,3} = 0;    cst{ixOAR,2} = 'OAR';       cst{ixOAR,3} = 'OAR';
+cst{ixOAR,1} = 2;    cst{ixOAR,2} = 'OAR';       cst{ixOAR,3} = 'OAR';
 
 % define optimization parameter for both VOIs
 cst{ixNT,5}.TissueClass = 1;
@@ -207,7 +207,7 @@ for ixRob = 1:numel(ROBUST_OPT)
    %cst{ixOAR,6}{1,2}.robustness   = 'COWC';
    
    resultGUIrobust = matRad_fluenceOptimization(dij,cst,pln);
-   
+    
    % combine resultGUI structures
    resultGUI = matRad_appendResultGUI(resultGUI,resultGUIrobust,0,['robust' ROBUST_OPT{1,ixRob}]);
    
